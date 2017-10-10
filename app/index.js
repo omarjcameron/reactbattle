c430768f09098eb8a4f77e737db3fdb6a7881d76
Omar Cameron <omarjcameron@gmail.com>
2017-10-09 22:23:32 -0700

    Add untracked files

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+var App = require('./components/App');
+
+
+// properties associated with components
+// state
+// lifecycle event
+// UI of component
+
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
